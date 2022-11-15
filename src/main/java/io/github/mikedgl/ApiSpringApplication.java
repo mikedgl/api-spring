7a28e3100623830f526b1f00f9381825c9825e61
Michael Douglas <mikedgls17@gmail.com>
2022-11-15 10:24:58 -0300

    projeto criado

---
+++ src/main/java/io/github/mikedgl/ApiSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.mikedgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringApplication.class, args);
+	}
+
+}
