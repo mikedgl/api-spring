2043b30b506dab80c0c9d2949c2efdc9b833ea44
Michael Douglas <mikedgls17@gmail.com>
2022-11-15 14:56:59 -0300

    import.sql

--- src/main/java/io/github/mikedgl/resources/CategoryResource.java
+++ src/main/java/io/github/mikedgl/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package io.github.mikedgl.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/io/github/mikedgl/services/CategoryService.java
+++ src/main/java/io/github/mikedgl/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.mikedgl.entities.Category;
 import io.github.mikedgl.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
